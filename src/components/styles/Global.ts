474ec08d77d0285a0386ed9f01a3833671bea587
MD Nelles <devstudioca@gmail.com>
2022-11-13 17:09:36 -0500

    auto - update

--- src/components/styles/Global.ts
+++ src/components/styles/Global.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 export const ObjWrapper = styled.div`
-   width: 100%;
-   height: 100%;
-   position: relative;
-   transform-style: preserve-3d;
+  width: 100%;
+  height: 100%;
+  position: relative;
+  transform-style: preserve-3d;
 `;
