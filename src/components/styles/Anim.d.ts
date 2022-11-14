a164c78362a85ec48d5e3d9dc9fc24f45018199f
mdnelles <mxnelles@gmail.com>
2022-11-14 14:06:06 +0000

    auto - update

--- src/components/styles/Anim.d.ts
+++ src/components/styles/Anim.d.ts
@@ -1,16 +1,16 @@
 export interface AnimStylesProps {
-   children?: any;
-   animSpecs?: any;
+  children?: any;
+  animSpecs?: any;
 }
 export interface AnimSpecsProps {
-   border?: string | undefined;
-   degreesHi?: number | undefined;
-   degreesLow?: number | undefined;
-   delay: any;
-   direction?: string | undefined;
-   duration?: number | string | undefined;
-   fillMode?: string | undefined;
-   iterationCount?: number | string | undefined;
-   name?: string | undefined;
-   timing?: string | undefined;
+  border?: string | undefined;
+  degreesHi?: number | undefined;
+  degreesLow?: number | undefined;
+  delay: any;
+  direction?: string | undefined;
+  duration?: number | string | undefined;
+  fillMode?: string | undefined;
+  iterationCount?: number | string | undefined;
+  name?: string | undefined;
+  timing?: string | undefined;
 }
